15d59d0849a410f01b0c012cb61e9fc656e3594d
Pavel Slyaz <sir.sevendart@gmail.com>
2021-10-06 23:36:50 +0300

    Added project with dedicated generators for loading with assembly

---
+++ DedicatedGenerators/FloatGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FakerLibrary.Generators;
+
+namespace DedicatedGenerators
+{
+    public class FloatGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext generatorContext)
+        {
+            var result = (float) generatorContext.Random.NextDouble();
+            var power = Math.Pow(10, generatorContext.Random.Next(0, 31));
+            return result * power;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(float);
+        }
+    }
+}
---
+++ DedicatedGenerators/LongGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FakerLibrary.Generators;
+
+namespace DedicatedGenerators
+{
+    public class LongGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext generatorContext)
+        {
+            var upperBits = generatorContext.Random.Next();
+            var lowerBits = generatorContext.Random.Next();
+            var result = ((long) upperBits << 32) | (uint) lowerBits;
+            return result;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(long);
+        }
+    }
+}
