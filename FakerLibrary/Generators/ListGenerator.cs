2b124fc12ec7a44d97d998fd76bf23b975cb5465
Pavel Slyaz <sir.sevendart@gmail.com>
2021-10-06 23:52:14 +0300

    Created IList generator

---
+++ FakerLibrary/Generators/ListGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FakerLibrary.Generators
+{
+    public class ListGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext generatorContext)
+        {
+            var targetType = generatorContext.Targets.Peek();
+            var genericTypeArgument = targetType.GenericTypeArguments[0];
+            var enumerable = Activator.CreateInstance(targetType);
+
+            var length = generatorContext.Random.Next(1000000);
+
+            var method = typeof(IFaker).GetMethod("Create").MakeGenericMethod(genericTypeArgument);
+            for (int i = 0; i < length; i++)
+            {
+                var obj = new []{
+                    method.Invoke(generatorContext.Faker, null)
+                };
+
+                targetType.GetMethod("Add")?.Invoke(enumerable, obj);
+            }
+
+            return enumerable;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            var interfaces = type.GetInterfaces();
+            return interfaces.Contains(typeof(IList)) && type.IsGenericType;
+        }
+    }
+}
